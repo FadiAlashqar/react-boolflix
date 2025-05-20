7eb3ae340b4f8c97d58532309336784de1dc4e36
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-20 21:59:36 +0200

    [UPD] search bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 d-flex align-items-center">
+              <h1>BOOLFLIX</h1>
+              <div className="form mx-3 d-flex">
+                <input className="form-control" type="text" />
+                <button type="submit" className="btn btn-outline-danger mx-2 fw-bold">Search...</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
