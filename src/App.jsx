b407995ce5099dff3f05a5c535a7ae5a86ae357b
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-20 23:14:54 +0200

    [UPD] milestone 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,11 @@ function App() {
   const getFilms = () => {
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c55ebc7d636fbf64fb69c83bfa8d0c7e&query=${film}`).then((resp) => {
       setSelectedFilm(resp.data);
+      console.log(resp.data)
     })
   }
+
+  const results = selectedFilm.results;
 
   return (
     <>
@@ -29,19 +32,21 @@ function App() {
       </header>
       <main>
         <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-6 col-lg-4">
-              <div className="card">
-                <div className="card-img-top">
-                  <img src="" alt="" />
-                </div>
-                <div className="card-body">
-                  <div className="card-text">
-                    <p></p>
+          <div className="row g-4">
+            {results?.map((result, index) => {
+              return <div key={index} className="col-12 col-md-6 col-lg-4">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="card-text">
+                      <h4>Title : {result.title}</h4>
+                      <h5>Original Title : {result.original_title}</h5>
+                      <p>Language : {result.original_language}</p>
+                      <p>Rate : {result.vote_average}</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            })}
           </div>
         </div>
       </main>
